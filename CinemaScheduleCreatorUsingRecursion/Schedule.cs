291cfe094efc8b6bab5336489d1ef54e80379479
Serhii Voloshyn <sergey.voloshyn97@gmail.com>
2021-05-19 20:15:30 +0300

    Working on schedule class

--- CinemaScheduleCreatorUsingRecursion/Schedule.cs
+++ CinemaScheduleCreatorUsingRecursion/Schedule.cs
@@ -4,7 +4,73 @@ using System.Text;
 
 namespace CinemaScheduleCreatorUsingRecursion
 {
-    class Schedule
+    public class Schedule
     {
+        public static List<Movie> Movies { get; set; }
+        public List<Movie> MoviesInSchedule { get; set; }
+        public int FreeTime { get; set; }
+        public int AllMoviesCount { get; }
+
+        public Schedule(int freeTime, List<Movie> moviesInSchedule = null)
+        {
+            FreeTime = freeTime;
+
+            if (moviesInSchedule is null)
+            {
+                MoviesInSchedule = new List<Movie>();
+            }
+            else
+            {
+                MoviesInSchedule = moviesInSchedule;
+            }
+        }
+
+        public void CreateSchedule()
+        {
+            Schedule currentSchedule;
+            Schedule bestSchedule;
+
+            foreach (var movie in Movies)
+            {
+                if (FreeTime >= movie.RunningTimeInMinutes)
+                {
+                    List<Movie> tmp = new List<Movie>(MoviesInSchedule);
+                    tmp.Add(movie);
+                    currentSchedule = new Schedule(FreeTime - movie.RunningTimeInMinutes, tmp);
+
+                    if (CheckBestScheduleCriteriaAccordance(currentSchedule))
+                    {
+                        bestSchedule = currentSchedule;
+                    }
+
+                    currentSchedule.CreateSchedule();
+                }
+            }
+        }
+
+        public bool CheckBestScheduleCriteriaAccordance(Schedule schedule)
+        {
+            bool result = false;
+            int uniqueMoviesCount = 0;
+
+            foreach(Movie movie in Movies)
+            {
+                if(schedule.MoviesInSchedule.Contains(movie))
+                {
+                    uniqueMoviesCount++;
+                }
+            }
+
+            return result;
+        }
+
+        private bool CheckIfAllMoviesCouldFitInSchedule()
+        {
+            bool result = false;
+
+            
+
+            return result;
+        }
     }
 }
