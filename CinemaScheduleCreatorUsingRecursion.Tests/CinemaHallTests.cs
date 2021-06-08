031f5ce71120a89f8c7625ce74c95fc68b85904f
Serhii Voloshyn <sergey.voloshyn97@gmail.com>
2021-06-08 06:06:37 +0300

    wrote one simple test for CinemaHall recursive method

--- CinemaScheduleCreatorUsingRecursion.Tests/CinemaHallTests.cs
+++ CinemaScheduleCreatorUsingRecursion.Tests/CinemaHallTests.cs
@@ -1,19 +1,66 @@
 using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace CinemaScheduleCreatorUsingRecursion.Tests
 {
     public class CinemaHallTests
     {
-        [SetUp]
-        public void Setup()
+        [TestCaseSource(typeof(CreateScheduleTestSource))]
+        public void CreateSchedule_WhenGivenSchedule_ShouldReturnBestSchedule(Schedule schedule, Schedule expectedSchedule, CinemaHall cinemaHall)
         {
+            Schedule actualSchedule = cinemaHall.CreateSchedule(schedule);
 
+            foreach (Movie movie in expectedSchedule.MoviesInSchedule)
+            {
+                if (!(actualSchedule.MoviesInSchedule.Contains(movie)))
+                {
+                    Assert.Fail();
+                }
+                else
+                {
+                    if (actualSchedule.FreeTime == expectedSchedule.FreeTime)
+                    {
+                        Assert.Pass();
+                    }
+                    else
+                    {
+                        Assert.Fail();
+                    }
+                }
+
+            }
         }
 
-        [Test]
-        public void Test1()
+        public class CreateScheduleTestSource : IEnumerable
         {
-            Assert.Pass();
+            List<Movie> Movies;
+            CinemaHall CinemaHall;
+            Schedule EmptySchedule;
+            Schedule ResultSchedule;
+
+            public IEnumerator GetEnumerator()
+            {
+                Movies = new List<Movie>()
+                {
+                    Movie.CreateMovie("AAA", 5),
+                    Movie.CreateMovie("BBB", 10),
+                    Movie.CreateMovie("CCC", 15),
+                    Movie.CreateMovie("DDD", 20)
+                };
+
+                CinemaHall = CinemaHall.CreateCinemaHall(Movies, 50);
+                EmptySchedule = new Schedule();
+                ResultSchedule = new Schedule();
+                ResultSchedule.MoviesInSchedule = new List<Movie>(Movies);
+
+                yield return new object[]
+                {
+                    EmptySchedule,
+                    ResultSchedule,
+                    CinemaHall
+                };
+            }
         }
     }
 }
